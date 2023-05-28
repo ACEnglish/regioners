a04bd8cd8fa18305fd62646116a51ad875fcc89f
Adam English <ACEnglish@gmail.com>
2023-05-28 10:45:03 -0400

    Switch overlappers
    any / all had flipped functionality

--- src/overlappers.rs
+++ src/overlappers.rs
@@ -16,18 +16,18 @@ pub enum Overlapper {
 impl Overlapper {
     pub fn ovl(&self, a_intv: &Lapper<u64, u64>, b_intv: &Lapper<u64, u64>) -> u64 {
         match self {
-            /* Return number of A intervals intersecting a B intervals */
+            /* Return number of B intervals intersecting each A interval */
             Overlapper::All => a_intv
+                .iter()
+                .map(|i| b_intv.find(i.start, i.stop).count() as u64)
+                .sum(),
+            /* Return number of A intervals intersecting a B intervals */
+            Overlapper::Any => a_intv
                 .iter()
                 .map(|i| match b_intv.find(i.start, i.stop).next() {
                     Some(_) => 1,
                     None => 0,
                 })
-                .sum(),
-            /* Return number of b intervals intersecting each of a's intervals */
-            Overlapper::Any => a_intv
-                .iter()
-                .map(|i| b_intv.find(i.start, i.stop).count() as u64)
                 .sum(),
         }
     }
